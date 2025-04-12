8668ce8aec24c70872a0391817e5e5c0e3b12a71
vlad malygin <vlad.malygin.02@mail.ru>
2025-04-12 23:04:49 +0300

    admin update

--- backend/memories/models.py
+++ backend/memories/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 class GuestbookEntry(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    role = models.CharField(max_length=100)
-    memory = models.TextField()
-    photo = models.TextField(blank=True, null=True)
+    first_name = models.CharField(max_length=100, verbose_name='Имя')
+    last_name = models.CharField(max_length=100, verbose_name='Фамилия')
+    role = models.CharField(max_length=100, verbose_name='Роль')
+    memory = models.TextField(verbose_name='Воспоминание')
+    photo = models.TextField(blank=True, null=True, verbose_name='Фото')
+
+    class Meta:
+        verbose_name = 'Запись в гостевой книге'
+        verbose_name_plural = 'Записи в гостевой книге'
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-
