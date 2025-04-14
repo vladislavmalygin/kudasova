5db8b88dbc0491610c168d817e8c9b8698c3f9b3
vlad malygin <vlad.malygin.02@mail.ru>
2025-04-14 08:33:44 +0300

    content admin

--- backend/content/admin.py
+++ backend/content/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Image
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+
