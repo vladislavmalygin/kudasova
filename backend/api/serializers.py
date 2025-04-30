60e45ee5382b14f902c6b44473f38b6fff35a37a
vladislavmalygin <vlad.malygin.02@mail.ru>
2025-04-30 17:19:43 +0300

    image encoding

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,5 @@
+import base64
+
 from rest_framework import serializers
 
 from content.models import Image, Video, Poem
@@ -5,9 +7,18 @@ from memories.models import GuestbookEntry, Memory
 
 
 class ImageSerializer(serializers.ModelSerializer):
+    image_base64 = serializers.SerializerMethodField()
+
     class Meta:
         model = Image
-        fields = ['id', 'image', 'description']
+        fields = ['id', 'image', 'description', 'image_base64']
+
+    def get_image_base64(self, obj):
+        if obj.image:
+            with open(obj.image.path, "rb") as image_file:
+                encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+                return f"data:image/jpeg;base64,{encoded_string}"
+        return None
 
 
 class GuestbookEntrySerializer(serializers.ModelSerializer):
