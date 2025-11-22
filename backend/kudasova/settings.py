ec3ae8d87e8ce5d5a6f673e3f5a9d0447e09073a
vlad malygin <vlad.malygin.02@mail.ru>
2025-11-22 12:42:10 +0300

    static root

--- backend/kudasova/settings.py
+++ backend/kudasova/settings.py
@@ -115,7 +115,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
