83c7df72f026319329178d4ac03aebe10e0b605c
vlad malygin <vlad.malygin.02@mail.ru>
2025-11-22 12:28:15 +0300

    dockerfile backend update

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,6 +4,6 @@ echo 'Running migrations...'
 python manage.py migrate
 echo 'Loading data...'
 python manage.py load_data
-gunicorn foodgram.wsgi --bind 0.0.0.0:8080
+gunicorn kudasova.wsgi --bind 0.0.0.0:8080
 
 exec "$@"
