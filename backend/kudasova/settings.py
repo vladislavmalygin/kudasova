92a3b2176417bf6b07b9edfca7d6461abde14243
vlad malygin <vlad.malygin.02@mail.ru>
2025-11-22 11:04:50 +0300

    try 56

--- backend/kudasova/settings.py
+++ backend/kudasova/settings.py
@@ -1,13 +1,124 @@
-SECRET_KEY = secret key
+from pathlib import Path
+import os
 
-DEBUG=True
+from django.core.management.utils import get_random_secret_key
 
-ALLOWED_HOSTS=localhost,127.0.0.1,https://kudasova.com,195.133.48.36
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-CSRF_TRUSTED_ORIGINS=http://localhost,http://127.0.0.1,https://kudasova.com,http://195.133.48.36
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', get_random_secret_key())
 
-POSTGRES_USER=django
-POSTGRES_PASSWORD=django
-POSTGRES_DB=django
-DB_HOST=db
-DB_PORT=5432
+DEBUG = os.getenv('DEBUG', '').lower() in ('true', 'on', '1')
+# Установите ALLOWED_HOSTS в пустой список по умолчанию
+ALLOWED_HOSTS = (
+    os.getenv('ALLOWED_HOSTS', 'localhost, 127.0.0.1')
+    .replace(' ', '')
+    .split(',')
+)
+if 'CSRF_TRUSTED_ORIGINS' in os.environ:
+    CSRF_TRUSTED_ORIGINS = (
+        os.getenv('CSRF_TRUSTED_ORIGINS').replace(' ', '').split(',')
+    )
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    #second_apps
+    'rest_framework',
+    'corsheaders',
+    #project_apps
+    'memories',
+    'content',
+    'api'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+ROOT_URLCONF = 'kudasova.urls'
+
+TEMPLATES_DIR = BASE_DIR / 'templates'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'kudasova.wsgi.application'
+
+USE_SQLITE = os.getenv('USE_SQLITE', 'False').lower() in ['True']
+
+if USE_SQLITE:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB', 'django'),
+            'USER': os.getenv('POSTGRES_USER', 'django'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'django'),
+            'HOST': os.getenv('DB_HOST', 'db'),
+            'PORT': os.getenv('DB_PORT', 5432),
+        }
+    }
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'ru'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
